77f25f6f769624b5d325f2b2298cd09e4d63ebee
Readpato <87768087+Readpato@users.noreply.github.com>
2021-08-26 16:25:48 +0200

    Agregado Boton Calcular Promedio Array
    Agregado el codigo que se ejecuta cuando se clickea sobre el boton que calcula el promedio de un array declarado en <ol> y <li>.

--- Tarea 5c/Tarea 5c JS.js
+++ Tarea 5c/Tarea 5c JS.js
@@ -7,4 +7,24 @@
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
 
+//Acordarse que si queremos llamar a un elemento con el querySelector y el mismo tiene una clase o id, tenemos que usar los
+// simbolos correspondientes para llamarlos.
+
+
+const $botonCalcularPromedio = document.querySelector('#calcularPromedio');
+
+$botonCalcularPromedio.onclick = function() {
+    const $numerosArray = document.querySelectorAll('.numerosArray');
+    const $calculoPromedioArray = document.querySelector('#calculoPromedioArray');
+    let sumaTotalNumerosArray = 0;
+
+    for (i = 0; i < $numerosArray.length; i++) {
+        sumaTotalNumerosArray = sumaTotalNumerosArray + Number($numerosArray[i].innerText);
+        
+    }
+
+    let promedioTotal = sumaTotalNumerosArray / $numerosArray.length;
+    $calculoPromedioArray.innerText = promedioTotal;
+    return false
+}
 
